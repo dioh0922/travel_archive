5a5e2e2f736f95bb00516a64af77e62c35ef244e
dioh0922 <diohound@gmail.com>
2022-10-28 11:00:58 +0900

    アクセス数の制御

---
+++ src/Access.php
@@ -0,0 +1,30 @@
+<?php
+namespace Src;
+use ORM;
+use Dotenv;
+class Access{
+	public function __construct(){
+		$env = Dotenv\Dotenv::createImmutable(dirname(__FILE__)."/../../env");
+		$env->load();
+		ORM::configure("mysql:host=localhost;charset=utf8;dbname=".$_ENV["DB_DB"]);
+		ORM::configure("username", $_ENV["DB_USER"]);
+		ORM::configure("password", $_ENV["DB_PASS"]);
+	}
+
+	public function getCurrentCount(string $today){
+		return ORM::for_table("g_access_count")->select("access_count")->where("access_day", $today)->find_one();
+	}
+	public function saveAccess(string $today){
+		$access = ORM::for_table("g_access_count")->create();
+		$access->access_day = $today;
+		$access->save();
+		return $access->id();
+	}
+	public function addAccess(string $today){
+		$access = ORM::for_table("g_access_count")->select("*")->where("access_day", $today)->find_one();
+		$access->access_count++;
+		$access->save();
+	}
+}
+
+?>
